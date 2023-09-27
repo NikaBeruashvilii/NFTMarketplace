ec25830e4a953a3c5a599ca0d0b6a58b3bb8e904
NikaBeruashvilii <nika.beruashvili.1@btu.edu.ge>
2023-09-27 20:10:59 +0400

    Deleted stashes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { XyzComponent } from './xyz/xyz.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 import { AboutComponent } from './about/about.component';
@@ -11,7 +10,6 @@ import { AboutComponent } from './about/about.component';
 @NgModule({
   declarations: [
     AppComponent,
-    XyzComponent,
     HeaderComponent,
     ContentComponent,
     AboutComponent
