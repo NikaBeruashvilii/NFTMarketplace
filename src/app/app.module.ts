e4361e8a024a272f6cdd455144736d0c66fd6055
KetiElizbarashvili <elizbarashviliketi44@gmail.com>
2023-09-27 16:44:43 +0400

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { XyzComponent } from './xyz/xyz.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    XyzComponent
+    XyzComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
